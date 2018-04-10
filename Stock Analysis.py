3ecab5dab1eb518e78e7d0d18c8db496d60bdf69
mattrdurante <mattrdurante@gmail.com>
2018-04-09 21:02:59 -0400

    Update Stock Analysis.py

--- Stock Analysis.py
+++ Stock Analysis.py
@@ -49,13 +49,12 @@ for df in s:
     df['stdev'] = df['Close'].rolling(window=20,min_periods=10).std()
     df['20ma'] = df['Close'].rolling(window=20,min_periods=0).mean()
     df['20ewm'] = df['Close'].ewm(span=20,adjust=True).mean()
+    df['mark'] = df['Close'] - df['20ewm']
 
 #initialize empty array to store key value (stock symbol) of each dataframe for plot titles
 plottitles = []
 for key in s:
     plottitles.append(key)
-
-benchmark = s['SPX']
 
     
 #create counter at 0 to iterate through keys
@@ -63,7 +62,7 @@ i = 0
 
 for df in s: 
     df = s[df]
-    df[['Close','20ma','20ewm']].plot()
+    df[['Close','20ma','20ewm','mark']].plot()
     plt.title(plottitles[i], fontsize=20)
     plt.tight_layout()
     plt.savefig(str(plottitles[i])+' 2017-To-Date.png', dpi = 300)
